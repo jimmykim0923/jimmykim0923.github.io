24bf86530d13174c0873990931a7400f2fab7b7d
jimmykim0923 <151721497+jimmykim0923@users.noreply.github.com>
2023-11-22 18:21:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
+// 여기에 JavaScript 코드를 넣어주세요
 function handleOCR() {
-  const fileInput = document.getElementById('fileInput');
+  const fileInput = document.createElement('input');
+  fileInput.type = 'file';
+  fileInput.accept = 'image/*';
   fileInput.addEventListener('change', async (event) => {
     const file = event.target.files[0];
-    const imageURL = URL.createObjectURL(file);
-
-    // 이미지 표시
-    const uploadedImage = document.getElementById('uploaded-image');
-    uploadedImage.src = imageURL;
-    uploadedImage.style.display = 'block';
-
     const { data: { text } } = await Tesseract.recognize(
       file,
       'kor',
       { logger: m => console.log(m) }
     );
     console.log('인식된 텍스트:', text);
-    document.querySelector('.result-container #recognized-text').innerText = text;
+    document.querySelector('.result-container').innerHTML = `<p>${text}</p>`;
   });
+  fileInput.click();
 }
